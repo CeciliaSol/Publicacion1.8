91c808118ca297e846e5838a3f7c3e4c690be84d
CeciliaSol <ceciliasolheredia@gmail.com>
2019-10-30 22:12:20 -0300

    urlConf - primera vista

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^polls/', include('polls.urls')),
 ]
